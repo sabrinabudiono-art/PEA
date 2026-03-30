7b5b9e6bf706be6beb9f6f6702fd023c7b23efc8
Sabrina <sabrinabudiono@gmail.com>
2026-03-30 17:04:37 +0200

    added pdf_processor.py

---
+++ pdf_processor.py
@@ -0,0 +1,14 @@
+import sys
+
+import pymupdf4llm
+
+def extract_text_from_pdf(pdf_path: str) -> str:
+    """
+    Extract text from PDF file
+    :param pdf_path:
+    :return: Markdown text of the PDF file
+    """
+    markdown_text = pymupdf4llm.to_markdown(pdf_path)
+    return markdown_text
+
+print(extract_text_from_pdf("uploads/Rechnung.pdf"))
